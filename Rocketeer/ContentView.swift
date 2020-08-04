4694769ffc50d9357e0e4ee63f6c8dfe5736ad8b
Noah Wilder <noahleviwilder@gmail.com>
2020-08-04 13:14:30 -0400

    Added necessary functions to load launch data.

--- Rocketeer/ContentView.swift
+++ Rocketeer/ContentView.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    let url = URL(string: "https://spaceflightnow.com/launch-schedule/")
+    @State var missions: [Mission] = []
+    
     var body: some View {
         Text("Hello, world!").padding()
+    }
+    
+    func loadLaunchData() {
+        var contents = ""
+        do {
+            try contents = String(contentsOf: url!)
+        } catch {
+            return
+        }
+        let matches = contents.matches(pattern: #"(?<=<div class="datename"><span class="launchdate">)(.|\n)+?<div class="missdescrip">(.|\n)+?</div>"#)
+        for match in matches {
+            let date = String(match.prefix { $0 != "<" }).removeHtmlSpecialCharacters()
+            
+            let name = String(match[match.range(of: #"(?<=class="mission">).+?(?=</span)"#, options: .regularExpression)!]).removeHtmlSpecialCharacters().components(separatedBy: " • ")
+            
+            let rocket = name[0]
+            let payload = name[1]
+            let launchTime = String(match[match.range(of:  #"(?<=Launch (window|time):</span> ).+?(?=<span)"#, options: .regularExpression)!]).removeHtmlSpecialCharacters().replacingOccurrences(of: #"(\d{2})(\d{2})"#, with: "$1:$2", options: .regularExpression)
+            let launchSite = String(match[match.range(of:  #"(?<=Launch site:</span> ).+?(?=</div>)"#, options: .regularExpression)!]).removeHtmlSpecialCharacters()
+            // Removing the updates
+            let missionDescription = String(match[match.range(of:  #"(?<=<div class="missdescrip">).+?(?=(\[<span|</div>|</p>))"#, options: .regularExpression)!]).removeHtmlSpecialCharacters()
+            
+            
+            let mission = Mission(date: date,
+                                  rocket: rocket,
+                                  payload: payload,
+                                  launchTime: launchTime,
+                                  launchSite: launchSite,
+                                  description: missionDescription)
+            
+            missions.append(mission)
+            print(mission)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+fileprivate extension String {
+    func removeHtmlSpecialCharacters() -> String {
+        var str = replacingOccurrences(of: "&#8217;", with: "'")
+        str = str.replacingOccurrences(of: "&#038;", with: "&")
+        str = str.replacingOccurrences(of: "&#8220;", with: "\"")
+        str = str.replacingOccurrences(of: "&#8221;", with: "\"")
+        str = str.replacingOccurrences(of: "<(.+?)>(.+?)</\\1>", with: "$2", options: .regularExpression)
+        return str
     }
 }
