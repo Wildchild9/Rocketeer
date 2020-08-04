b6a9b70187b6051d6e3c10c6b6df5210404f4b10
Noah Wilder <noahleviwilder@gmail.com>
2020-08-04 12:41:07 -0400

    Created Xcode project.

---
+++ Rocketeer/Rocketeer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Rocketeer
+//
+//  Created by Noah Wilder on 2020-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rocketeer/Rocketeer/RocketeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketeerApp.swift
+//  Rocketeer
+//
+//  Created by Noah Wilder on 2020-08-04.
+//
+
+import SwiftUI
+
+@main
+struct RocketeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
