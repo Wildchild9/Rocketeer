f045a642f5835d14f4db9257e86ec0183fd5d7f1
Noah Wilder <noahleviwilder@gmail.com>
2020-08-04 13:04:00 -0400

    Added regular expression matching extension to `String`.

---
+++ Rocketeer/Rocketeer/Extensions/String+matches.swift
@@ -0,0 +1,29 @@
+//
+//  String+matches.swift
+//  Rocketeer
+//
+//  Created by Noah Wilder on 2020-08-04.
+//
+
+import Foundation
+
+extension String {
+    func matches<Target: StringProtocol>(pattern regex: Target, options: NSRegularExpression.MatchingOptions = []) -> [Substring] {
+        return matches(pattern: regex, options: options, in: startIndex..<endIndex)
+    }
+    
+    func matches<Target: StringProtocol, Region: RangeExpression>(pattern regex: Target, options: NSRegularExpression.MatchingOptions = [], in region: Region) -> [Substring] where Region.Bound == Index {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: String(regex))
+            
+            let results = regex.matches(in: self, options: options, range: NSRange(region.relative(to: self), in: self))
+            
+            let finalResult = results.map { self[Range($0.range, in: self)!] }
+            return finalResult
+        } catch let error {
+            print("Invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
