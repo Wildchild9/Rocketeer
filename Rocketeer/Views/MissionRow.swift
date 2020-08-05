75b5cb7c6f3a3b38bd41d4a9a1867fb2827d4bd3
Jacob Tepperman <jtepp@icloud.com>
2020-08-05 17:38:54 -0400

    Resized padding and spacers

--- Rocketeer/Views/MissionRow.swift
+++ Rocketeer/Views/MissionRow.swift
@@ -9,36 +9,44 @@ import SwiftUI
 
 struct MissionRow: View {
     var mission: Mission
-    
+	let space: CGFloat = 40
     var body: some View {
-        NavigationLink(destination: MissionView(mission: mission)) {
-            HStack {
-				mission.logo
-					.resizable()
-					.scaledToFit()
-					.frame(width: 45, height: 45)
-					.padding(2)
-					.background(
-						RoundedRectangle(cornerRadius: 5)
-							.fill(Color.white)
-							.shadow(color: Color.black.opacity(0.3), radius: 2)
-					)
+		HStack {
+			Spacer(minLength: self.space)
+			
+			NavigationLink(destination: MissionView(mission: mission)) {
+				HStack {
+					mission.logo
+						.resizable()
+						.scaledToFit()
+						.frame(width: 45, height: 45)
+						.padding(2)
+						.background(
+							RoundedRectangle(cornerRadius: 5)
+								.fill(Color.white)
+								.shadow(color: Color.black.opacity(0.3), radius: 2)
+						)
 
-                VStack(alignment: .leading) {
-                    Text(mission.rocket)
-						.bold()
-						.foregroundColor(Color.white)
-                    
-                    Text(mission.payload)
-                        .foregroundColor(Color("white-gray"))
-                        .font(.subheadline)
-                }
-                Spacer()
-                Text(mission.date).foregroundColor(Color("white-gray"))
-            }
+					VStack(alignment: .leading) {
+						Text(mission.rocket)
+							.bold()
+							.foregroundColor(Color.white)
+						
+						Text(mission.payload)
+							.foregroundColor(Color("white-gray"))
+							.font(.subheadline)
+					}
+					Spacer()
+					Text(mission.date).foregroundColor(Color("white-gray"))
+				}
+				Spacer(minLength: self.space)
+			}
+			.buttonStyle(PlainButtonStyle())
+			.padding(.horizontal, 20)
+			.padding(.vertical, 20)
+			.background(RoundedRectangle(cornerRadius: 30))
+			
+			Spacer(minLength: self.space)
 		}
-		.buttonStyle(PlainButtonStyle())
-		.padding(.horizontal, 50)
-		.padding(.vertical, 20)
     }
 }
