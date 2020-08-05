7f0ffcea74fd5c5f6f7838f883b57856ceefde39
Jacob Tepperman <jtepp@icloud.com>
2020-08-05 16:02:53 -0400

    Added `Star` button to `NavigationToolbarItems` in `MissionView`

--- Rocketeer/Views/MissionView.swift
+++ Rocketeer/Views/MissionView.swift
@@ -11,18 +11,23 @@ struct MissionView: View {
     var mission: Mission
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            VStack(alignment: .leading, spacing: 15) {
-                Field(title: "Date", contents: mission.date)
-                Field(title: "Launch Time", contents: mission.launchTime)
-                Field(title: "Launch Site", contents: mission.launchSite)
-                Field(title: "Payload", contents: mission.payload)
-                Field(title: "Description", contents: mission.description)
-                
-                Spacer()
-            }
-        }
+		ScrollView(.vertical, showsIndicators: false) {
+			VStack(alignment: .leading, spacing: 15) {
+				Field(title: "Date", contents: mission.date)
+				Field(title: "Launch Time", contents: mission.launchTime)
+				Field(title: "Launch Site", contents: mission.launchSite)
+				Field(title: "Payload", contents: mission.payload)
+				Field(title: "Description", contents: mission.description)
+				
+				Spacer()
+			}
+		}
         .padding(20)
         .navigationTitle(mission.rocket)
+		.navigationBarItems(trailing:
+			Button(action: {print("star")}, label: {
+				Image(systemName: "star.fill")
+			})
+			)
     }
 }
