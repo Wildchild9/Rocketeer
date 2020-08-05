d394e185650b78f4f691d918a15755a07ae05af0
Jacob Tepperman <jtepp@icloud.com>
2020-08-05 16:12:24 -0400

    Added a background to the mission logo in `MissionRow`.

--- Rocketeer/Views/MissionRow.swift
+++ Rocketeer/Views/MissionRow.swift
@@ -13,12 +13,16 @@ struct MissionRow: View {
     var body: some View {
         NavigationLink(destination: MissionView(mission: mission)) {
             HStack {
-                mission.logo
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 45, height: 45)
-                    .shadow(color: Color.black.opacity(0.3), radius: 2)
-                    .cornerRadius(5)
+				mission.logo
+					.resizable()
+					.scaledToFit()
+					.frame(width: 45, height: 45)
+					.padding(2)
+					.background(
+						RoundedRectangle(cornerRadius: 5)
+							.fill(Color.white)
+							.shadow(color: Color.black.opacity(0.3), radius: 2)
+					)
 
                 VStack(alignment: .leading) {
                     Text(mission.rocket)
