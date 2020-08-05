2edf29c808e3b908ae517beacff3e9dea940b474
Jacob Tepperman <jtepp@icloud.com>
2020-08-05 18:21:51 -0400

    Added card colors

--- Rocketeer/Views/MissionRow.swift
+++ Rocketeer/Views/MissionRow.swift
@@ -9,44 +9,51 @@ import SwiftUI
 
 struct MissionRow: View {
     var mission: Mission
-	let space: CGFloat = 40
+	let space: CGFloat = 30
     var body: some View {
-		HStack {
-			Spacer(minLength: self.space)
-			
-			NavigationLink(destination: MissionView(mission: mission)) {
-				HStack {
-					mission.logo
-						.resizable()
-						.scaledToFit()
-						.frame(width: 45, height: 45)
-						.padding(2)
-						.background(
-							RoundedRectangle(cornerRadius: 5)
-								.fill(Color.white)
-								.shadow(color: Color.black.opacity(0.3), radius: 2)
-						)
-
-					VStack(alignment: .leading) {
-						Text(mission.rocket)
-							.bold()
-							.foregroundColor(Color.white)
-						
-						Text(mission.payload)
-							.foregroundColor(Color("white-gray"))
-							.font(.subheadline)
-					}
-					Spacer()
-					Text(mission.date).foregroundColor(Color("white-gray"))
-				}
+		VStack {
+			HStack {
 				Spacer(minLength: self.space)
+				
+				NavigationLink(destination: MissionView(mission: mission)) {
+					VStack(alignment: .leading) {
+						Text(mission.date).foregroundColor(Color("white-gray"))
+							.font(.subheadline)
+						HStack(alignment: .top) {
+							mission.logo
+								.resizable()
+								.scaledToFit()
+								.frame(width: 45, height: 45)
+								.padding(2)
+								.background(
+									RoundedRectangle(cornerRadius: 5)
+										.fill(Color.white)
+										.shadow(color: Color.black.opacity(0.3), radius: 2)
+								)
+
+							VStack(alignment: .leading) {
+									Text(mission.rocket)
+										.bold()
+										.foregroundColor(Color("bg-card-text"))
+								
+								Text(mission.payload)
+									.foregroundColor(Color("white-gray"))
+									.font(.subheadline)
+							}
+						}
+					}
+					Spacer(minLength: self.space)
+				}
+				.buttonStyle(PlainButtonStyle())
+				.padding(.horizontal, 20)
+				.padding(.vertical, 20)
+				.background(RoundedRectangle(cornerRadius: 30).fill(Color("bg-card")))
+				
+				Spacer(minLength: self.space)
+				
 			}
-			.buttonStyle(PlainButtonStyle())
-			.padding(.horizontal, 20)
-			.padding(.vertical, 20)
-			.background(RoundedRectangle(cornerRadius: 30))
-			
 			Spacer(minLength: self.space)
 		}
+	
     }
 }
