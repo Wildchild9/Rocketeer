ea04cc95d55a3caac7a0709f61156bac13bf81aa
Noah Wilder <noahleviwilder@gmail.com>
2020-08-05 21:16:46 -0400

    Created `MissionEntry` to use as the timeline entry for the widgets.

---
+++ RocketeerWidget/MissionEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MissionEntry.swift
+//  Rocketeer
+//
+//  Created by Noah Wilder on 2020-08-05.
+//
+
+import WidgetKit
+
+struct MissionEntry: TimelineEntry {
+    let date: Date
+    let missions: [Mission]
+}
